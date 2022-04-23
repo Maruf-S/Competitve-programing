1c643dc4a623047c6cab172f31a69dd68b3ffb04
Maruf-S <se.maruf.sherefa@gmail.com>
2022-04-23 14:40:57 +0300

    question update week 1

--- Week 1/array-with-elements-not-equal-to-average-of-neighbors.py
+++ Week 1/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,15 +1,6 @@
-class Solution(object):
+class Solution:
     def rearrangeArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[inta
-        """
-        clear = False
-        while(clear==False):
-            clear = True
-            for i in range(1,len(nums)-1):
-                if(nums[i]==(nums[i-1]+nums[i+1])/2):
-                    nums[i-1],nums[i] = nums[i],nums[i-1]
-                    clear = False
-        return (nums) 
-        
+        nums.sort()
+        for i in range(0,len(nums)-1,2):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
