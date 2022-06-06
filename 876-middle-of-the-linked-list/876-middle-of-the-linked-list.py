d37030c8f96053a7d9ec23d8425af1b9d37e5341
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-06 23:25:17 +0300

    Time: 22 ms (62.04%), Space: 13.7 MB (7.77%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,20 +5,9 @@
 #         self.next = next
 class Solution(object):
     def middleNode(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        i = 0
-        ic = head
+        fast = slow = head
         
-        while(ic):
-            i+=1
-            ic = ic.next
-        j = (i//2)+1
-        i = 1
-        print(j)
-        while(i<j):
-            head= head.next
-            i+=1
-        return head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
