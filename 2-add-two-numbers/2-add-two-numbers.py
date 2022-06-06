d4d3a08877944bc0667aba2ba04245a31e156927
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-06 23:06:19 +0300

    Time: 54 ms (87.60%), Space: 13.4 MB (89.76%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,30 +1,19 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-    def addTwoNumbers(self, l1, l2, carry=0):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
-        if l1 is None and l2 is None and carry == 0:
-            return None
-        
-        val_1 = 0 if l1 is None else l1.val
-        val_2 = 0 if l2 is None else l2.val
-        
-        val_sum = val_1 + val_2 + carry
-        next_carry = 1 if val_sum > 9 else 0
-        
-        digit = val_sum % 10
-        result_node = ListNode(digit)
-        
-        next_1 = None if l1 is None else l1.next
-        next_2 = None if l2 is None else l2.next
-        
-        result_node.next = self.addTwoNumbers(next_1, next_2, next_carry)
-        return result_node
+     def addTwoNumbers(self, l1, l2):
+            carry = 0;
+            res = n = ListNode(0);
+            while l1 or l2 or carry:
+                if l1:
+                    carry += l1.val
+                    l1 = l1.next;
+                if l2:
+                    carry += l2.val;
+                    l2 = l2.next;
+                carry, val = divmod(carry, 10)
+                n.next = n = ListNode(val);
+            return res.next;
