7b96f89eac9346933f278b339317d5e8c9c631a6
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-08 05:53:03 +0300

    Time: 30 ms (57.85%), Space: 19 MB (5.16%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -1,20 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+    # Definition for singly-linked list.
+    # class ListNode(object):
+    #     def __init__(self, val=0, next=None):
+    #         self.val = val
+    #         self.next = next
 class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        prev = None
-        cur = head
-        while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-
-        return prev            
+    def reverseList(self,head):
+        if not head:
+            return None
+        # Head(top) of the to be reversed sub problem
+        newHead = head
+        if(head.next):
+            # There is a sub prob
+            # Think of 2->3->None on head(2) head(3) gets returned because after the subprob is reversed
+            # that is the top Node
+            newHead = self.reverseList(head.next)
+            # head.next was the 'next' element to the 'head' before the list was reversed
+            # but it is now the last element of the reversed list and it points to null
+            head.next.next = head
+            head.next = None
+        # If no sub prob return the only node or if there is a sub prob return the head
+        return newHead
+      
