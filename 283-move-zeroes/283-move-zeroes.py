51ab7c0e6a8c9d68a8562ac81e1315aa07ee1777
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-16 15:15:35 +0300

    Time: 244 ms (51.55%), Space: 15.8 MB (16.21%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,13 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        z = []
+        zero = 0  # records the position of "0"
         for i in range(len(nums)):
-            if(nums[i] ==0):
-                z.append(i)
-        pc = 0
-        for i in z:
-            nums.pop(i-pc)
-            pc+=1
-        nums.extend([0]*(len(z)))
-            
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
