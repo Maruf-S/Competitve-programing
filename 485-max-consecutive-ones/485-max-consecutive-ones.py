bb60ce1e2832d5e51a65aa11d9bee610054d177e
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-16 19:04:29 +0300

    Time: 352 ms (93.93%), Space: 14.4 MB (31.54%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -7,9 +7,10 @@ class Solution:
         while i < len(nums):
             if nums[i] == 1:
                 l = i
-                while i < len(nums) and nums[i] == 1:
+                while i + 1 < len(nums) and nums[i+1] == 1:
                     i += 1
-                ran = max(ran,i-l)
+                ran = max(ran,i-l+1)
+                i += 1
                 l = i
             else:
                 i +=1
