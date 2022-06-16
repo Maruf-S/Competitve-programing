0988c7b8591deb1dd28fccc3eb3e23c8baf2dbe9
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-16 15:15:48 +0300

    Time: 275 ms (39.79%), Space: 15.6 MB (63.75%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = 0  # records the position of "0"
+        zero = 0 
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[i], nums[zero] = nums[zero], nums[i]
