a4a367e5f5735547401924a3419e8f049ca3249c
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-06-30 12:08:53 +0300

    Time: 3171 ms (14.76%), Space: 60.3 MB (72.95%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -4,4 +4,5 @@ class Solution:
         for i in range(len(Q) - 1, -1, -1):
             points, jump = Q[i][0], Q[i][1]
             dp[i] = max(points + dp[min(jump + i + 1, len(Q))], dp[i + 1])
+            
         return dp[0]
