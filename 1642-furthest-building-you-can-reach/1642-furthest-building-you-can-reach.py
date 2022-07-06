235764ef184dfa39b2783e592d8281f158767724
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-06 15:22:22 +0300

    Time: 1055 ms (35.10%), Space: 28.6 MB (15.99%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -5,15 +5,42 @@ class Solution:
         while ( i < len(A) - 1):
             d = A[i + 1] - A[i]
             if d > 0:
-                heapq.heappush(heap,d)
-            # Exhausted ladders
-            if len(heap) > ladders:
-                smallest = heapq.heappop(heap)
-                bricks -= smallest
-            if bricks < 0:
-                return i
+                if d <= bricks:
+                    bricks -= d
+                    heapq.heappush(heap,-d)
+                elif ladders>0:
+                    if len(heap) > 0:
+                    # Recover max amnt of bricks 
+                        largest_leap = -heap[0]
+                        if(largest_leap > d):
+                            bricks += largest_leap
+                            heapq.heappop(heap)
+                            heapq.heappush(heap,-d)
+                            bricks -= d
+                    ladders-=1
+                else:
+                    break
             i+=1
         return i
+            
+
+# # Min heap solution
+# class Solution:
+#     def furthestBuilding(self, A, bricks, ladders):
+#         heap = []
+#         i = 0
+#         while ( i < len(A) - 1):
+#             d = A[i + 1] - A[i]
+#             if d > 0:
+#                 heapq.heappush(heap,d)
+#             # Exhausted ladders
+#             if len(heap) > ladders:
+#                 smallest = heapq.heappop(heap)
+#                 bricks -= smallest
+#             if bricks < 0:
+#                 return i
+#             i+=1
+#         return i
             
                 
         
