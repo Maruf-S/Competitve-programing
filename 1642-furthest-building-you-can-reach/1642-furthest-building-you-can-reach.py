82d775a3f9579c5f294514c70eeea8c8d42a419b
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-06 15:21:30 +0300

    Time: 1226 ms (17.51%), Space: 28.6 MB (15.99%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -5,21 +5,13 @@ class Solution:
         while ( i < len(A) - 1):
             d = A[i + 1] - A[i]
             if d > 0:
-                if d <= bricks:
-                    bricks -= d
-                    heapq.heappush(heap,-d)
-                elif ladders>0:
-                    if len(heap) > 0:
-                    # Recover max amnt of bricks 
-                        largest_leap = -heap[0]
-                        if(largest_leap > d):
-                            bricks += largest_leap
-                            heapq.heappop(heap)
-                            heapq.heappush(heap,-d)
-                            bricks -= d
-                    ladders-=1
-                else:
-                    break
+                heapq.heappush(heap,d)
+            # Exhausted ladders
+            if len(heap) > ladders:
+                smallest = heapq.heappop(heap)
+                bricks -= smallest
+            if bricks < 0:
+                return i
             i+=1
         return i
             
