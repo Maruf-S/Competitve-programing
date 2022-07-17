0ab6616f4651c13b909a95b2f11aaaa057a79953
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-18 00:51:57 +0300

    Time: 216 ms (21.04%), Space: 15.2 MB (90.29%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,25 +1,18 @@
 class Solution:
     def calculate(self, s):
-        def update(op, v):
-            if op == "+": stack.append(v)
-            if op == "-": stack.append(-v)
-            if op == "*": stack.append(stack.pop() * v)           #for BC II and BC III
-            if op == "/": stack.append(int(stack.pop() / v))      #for BC II and BC III
-    
-        it, num, stack, sign = 0, 0, [], "+"
-        
-        while it < len(s):
-            if s[it].isdigit():
-                num = num * 10 + int(s[it])
-            elif s[it] in "+-*/":
-                update(sign, num)
-                num, sign = 0, s[it]
-            elif s[it] == "(":                                        # For BC I and BC III
-                num, j = self.calculate(s[it + 1:])
-                it = it + j
-            elif s[it] == ")":                                        # For BC I and BC III
-                update(sign, num)
-                return sum(stack), it + 1
-            it += 1
-        update(sign, num)
-        return sum(stack)
+        total = 0
+        i, signs = 0, [1, 1]
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                start = i
+                while i < len(s) and s[i].isdigit():
+                    i += 1
+                total += signs.pop() * int(s[start:i])
+                continue
+            if c in '+-(':
+                signs += signs[-1] * (1, -1)[c == '-'],
+            elif c == ')':
+                signs.pop()
+            i += 1
+        return total
