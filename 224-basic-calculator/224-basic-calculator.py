8a693788d1752c7979a5050cd7727a8f403bf30f
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-18 10:49:37 +0300

    Time: 111 ms (73.46%), Space: 16 MB (16.94%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,27 +1,30 @@
 class Solution:
     def calculate(self, s):
-        def update(num,sign):
-            if sign == "+":
-                stack.append(num)
-            elif sign == "-":
-                stack.append(-num)
-        i, num, stack, sign = 0, 0, [], "+"
-        while i < len(s):
-            c = s[i]
-            if c.isdigit():
-                num = num*10 + int(c)
-            elif c in "+-":
-                update(num,sign)
-                num = 0
-                sign = c
-            elif c == "(":
-                res,j = self.calculate(s[i+1:])
-                update(res,sign)
-                num = 0
-                i = i + j 
-            elif c == ")":
-                update(num,sign)
-                return sum(stack), i + 1
-            i+=1
-        update(num,sign)
-        return sum(stack)
+        def calc(i):
+            def update(num,sign):
+                if sign == "+":
+                    stack.append(num)
+                elif sign == "-":
+                    stack.append(-num)
+            num, stack, sign = 0, [], "+"
+            while i < len(s):
+                c = s[i]
+                if c.isdigit():
+                    num = num*10 + int(c)
+                elif c in "+-":
+                    update(num,sign)
+                    num = 0
+                    sign = c
+                elif c == "(":
+                    res,j = calc(i+1)
+                    update(res,sign)
+                    num = 0
+                    i = j
+                    continue
+                elif c == ")":
+                    update(num,sign)
+                    return sum(stack), i + 1
+                i+=1
+            update(num,sign)
+            return sum(stack)
+        return calc(0)
