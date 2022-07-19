e7087cbc4f913c9082426bcc0175b7a3eeafc26e
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-20 00:14:17 +0300

    Time: 154 ms (61.93%), Space: 17.7 MB (73.76%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,18 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeKLists(self, lists) -> ListNode:
-        head = ListNode(None)
-        curr = head
-        h = []
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(h, (lists[i].val, i))
+    def mergeTwoSortedLists(self,l1,l2):
+                head = dummy = ListNode()
+                while l1 and l2:
+                    if l1.val < l2.val:
+                        dummy.next = l1
+                        l1 = l1.next
+                    else:
+                        dummy.next = l2
+                        l2 = l2.next
+                    dummy = dummy.next
+                if l1 or l2:
+                    dummy.next = l1 or l2
+                return head.next
 
-        while h:
-            val, i = heapq.heappop(h)
-            curr.next = ListNode(val)
-            curr = curr.next
-            if lists[i].next:
-                heapq.heappush(h, (lists[i].next.val, i))
-                lists[i] = lists[i].next
-
-        return head.next
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+                if len(lists) == 1:
+                    return lists[0]
+                if len(lists) == 0:
+                    return None
+                if len(lists) == 2:
+                    return self.mergeTwoSortedLists(lists[0],lists[1])
+                l,r = self.mergeKLists(lists[:len(lists)//2]),self.mergeKLists(lists[len(lists)//2:])
+                return self.mergeTwoSortedLists(l,r)
