76742372277d7a32652ac1c80d1d6ef9fbb418ee
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-19 17:43:34 +0300

    Time: 164 ms (19.90%), Space: 14.5 MB (36.02%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -6,8 +6,7 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i] < 0:
                 cur_max,cur_min = cur_min,cur_max
-                pass
             cur_max = max(cur_max* nums[i],nums[i])
             cur_min = min(cur_min*nums[i],nums[i])
-            global_max =  max(global_max,cur_max)
+            global_max =  max(global_max,cur_max,cur_min)
         return global_max
