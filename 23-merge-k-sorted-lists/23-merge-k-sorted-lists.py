a720c89815d1a18c565353338b461424dc3fa7e7
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-20 00:30:32 +0300

    Time: 201 ms (33.76%), Space: 17.9 MB (45.38%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,26 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoSortedLists(self,l1,l2):
-                head = dummy = ListNode()
-                while l1 and l2:
-                    if l1.val < l2.val:
-                        dummy.next = l1
-                        l1 = l1.next
-                    else:
-                        dummy.next = l2
-                        l2 = l2.next
-                    dummy = dummy.next
-                if l1 or l2:
-                    dummy.next = l1 or l2
-                return head.next
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-                if len(lists) == 1:
-                    return lists[0]
-                if len(lists) == 0:
-                    return None
-                if len(lists) == 2:
-                    return self.mergeTwoSortedLists(lists[0],lists[1])
-                l,r = self.mergeKLists(lists[:len(lists)//2]),self.mergeKLists(lists[len(lists)//2:])
-                return self.mergeTwoSortedLists(l,r)
+        h = []
+        for i,j in enumerate(lists):
+            if j:
+                heapq.heappush(h,(j.val,i,j))
+        dummy = head = ListNode()
+        while h:
+            v,i,n = heapq.heappop(h)
+            head.next = n
+            lists[i] = lists[i].next
+            if lists[i]:
+                heapq.heappush(h,(lists[i].val,i,lists[i]))
+            head = head.next
+        return dummy.next
