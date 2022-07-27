562c1a4bbda33ead103869dce7e8fc84594e7fab
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-07-27 12:22:07 +0300

    Time: 131 ms (51.70%), Space: 14.4 MB (37.17%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -3,12 +3,7 @@ class Solution:
         count1 = Counter(words1)
         count2 = Counter(words2)
         res = 0
-        if len(count1) > len(count2):
-            for k,v in count2.items():
-                if v == 1 and count1[k] == 1:
-                    res += 1
-        else:
-            for k,v in count1.items():
-                if v == 1 and count2[k] == 1:
-                    res += 1
+        for k,v in count2.items():
+            if v == 1 and count1[k] == 1:
+                res += 1
         return res
