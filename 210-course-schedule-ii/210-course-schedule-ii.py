b131e453a7e3b5ae2f90bb2492b4c7378c98d120
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-09 23:53:58 +0300

    Time: 1279 ms (5.01%), Space: 17.5 MB (28.13%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = defaultdict(list)
+        for course,prq in prerequisites:
+            prereq[course].append(prq)
+        res = []
+        visiting = {}
+        visited = {}
+        def dfs(c):
+            if c in visiting:
+                return False
+            if prereq[c] == []:
+                if c not in visited:
+                    res.append(c)
+                visited[c] = 1
+                return True
+            visiting[c] = 1
+            for i in prereq[c]:
+                if dfs(i) == False:
+                    return False
+            del visiting[c]
+            if c not in visited:
+                res.append(c)
+            visited[c] = 1
+            return True
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return []
+        return res
