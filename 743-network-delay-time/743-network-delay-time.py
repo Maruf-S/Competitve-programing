5fd1e6e7a344373a792e308a79aa3899db32516a
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-12 15:20:14 +0300

    Time: 473 ms (95.79%), Space: 16.8 MB (25.34%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -11,23 +11,7 @@ class Solution:
             if n1 not in visit:
                 t[n1] = w1
                 visit.add(n1)
-                for w,n in adj[n1]:
-                    heappush(q,(w + w1 ,n))
+                for w,v in adj[n1]:
+                    heappush(q,(w + w1 ,v))
         return max(t.values()) if len(visit) == n else -1
 
-class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj,visited = defaultdict(list),set()
-        q = [(0,k)]
-        t = {}
-        for u,v,w in times:
-            adj[u].append((w,v))
-        while q:
-            w1,n1 = heapq.heappop(q)
-            if n1 not in visited:
-                t[n1] = w1
-                visited.add(n1)
-                for w,v in adj[n1]:
-                    heapq.heappush(q,(w + w1,v))
-        return max(t.values()) if len(visited) == n else -1
-                    
