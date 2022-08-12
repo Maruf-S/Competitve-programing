16c25a42697b1b58f16e4be8bced67610f4cd2a3
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-12 16:42:42 +0300

    Time: 166 ms (83.43%), Space: 15.7 MB (9.29%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.py
@@ -0,0 +1,28 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        
+        timp = {}
+        adj = defaultdict(list)
+        for emp in employees:
+            adj[emp.id] = (emp.importance,emp.subordinates)
+        def dfs(eid):
+            if eid in timp:
+                return timp[eid]
+            val = 0
+            for i in adj[eid][1]:
+                val += dfs(i)
+            timp[eid] = val + adj[eid][0]
+            return timp[eid]
+        visit = set()
+        for item in employees:
+            dfs(item.id)
+        return timp[id]
