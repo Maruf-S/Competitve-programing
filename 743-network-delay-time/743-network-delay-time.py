283e4b466e6e815ab3f0b29388b8e636e558baa3
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-12 15:13:57 +0300

    Time: 858 ms (29.19%), Space: 16.8 MB (25.34%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,16 +1,35 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj = defaultdict(list)
-        q = deque([(0,k)])
-        t = [0] + [float("inf")] * n
+        for u,v,w in times:
+            adj[u].append((w,v))
+        t = {}
+        q = [(0,k)]
+        print(adj)
+        visit = set()
+        while q:
+            w1,n1 = heapq.heappop(q)
+            if n1 not in visited:
+                visit.add(n1)
+                for w,n in adj[n1]:
+                    if n not in visit:
+                        t[n] = w
+                        heappush(q,(w + w1 ,n))
+        print(t)
+        return max(t.values()) if len(t) == n else -1
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj,visited = defaultdict(list),set()
+        q = [(0,k)]
+        t = {}
         for u,v,w in times:
             adj[u].append((w,v))
         while q:
-            w1,n1 = q.popleft()
-            if w1 < t[n1]:
+            w1,n1 = heapq.heappop(q)
+            if n1 not in visited:
                 t[n1] = w1
+                visited.add(n1)
                 for w,v in adj[n1]:
-                    q.append((w + w1,v))
-        mx = max(t)
-        return mx if mx < float("inf") else -1
+                    heapq.heappush(q,(w + w1,v))
+        return max(t.values()) if len(visited) == n else -1
                     
