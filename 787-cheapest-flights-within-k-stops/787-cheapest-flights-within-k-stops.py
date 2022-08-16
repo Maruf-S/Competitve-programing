10bc30b6a4a408aefa249f52369d91e04a2f1638
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-16 19:27:03 +0300

    Time: 466 ms (19.07%), Space: 15.2 MB (53.65%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,22 +1,14 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = defaultdict(list)
-        for fr,to,price in flights:
-            adj[fr].append([to,price])
-        
-        stop = 0
-        t = {}
-        visit = set()
-        q = [(0,0,src)]
-        while q:
-            pr,st,node = heapq.heappop(q)
-            if node in t and t[node][1] <= st:
-                continue
-            if st - 1  > k:
-                continue
-            if node==dst:
-                return pr
-            t[node] = [pr,st]
-            for to,price in adj[node]:
-                heapq.heappush(q,(pr + price,st + 1,to))
-        return -1
+        prices = [float("inf") for i in range(n)]
+        prices[src] = 0
+        for i in range(k + 1):
+            tempPrices = prices[:]
+            for fro,to,price in flights:
+                if fro == float("inf"):
+                    continue
+                temp = prices[fro] + price
+                if temp < tempPrices[to]:
+                    tempPrices[to] = temp
+            prices = tempPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
