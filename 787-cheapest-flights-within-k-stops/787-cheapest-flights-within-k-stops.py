8ae2c5db1e97fdfc24cc6ace8e3b22c8b60983f9
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-16 18:00:23 +0300

    Time: 214 ms (57.99%), Space: 15.6 MB (17.44%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,45 +1,35 @@
 class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         adj = defaultdict(list)
-#         for fr,to,price in flights:
-#             adj[fr].append([to,price])
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for fr,to,price in flights:
+            adj[fr].append([to,price])
         
-#         stop = 0
-#         t = {}
-#         visit = set()
-#         q = [(0,src,0)]
-        
-#         while q:
-#             st,pr,dest = heapq.heappop(q)
-#             t[dest] = [pr,st]
-#             for to,price in adj[dest]:
-#                 if to not in t:
-#                     heapq.heappush(q,(st + 1,pr + price,to))
-#                 else:
-#                     if st  + 1 > k + 1:
-#                         pass
-#                     else:
-#                         if pr + price < t[to][0]:
-#                             heapq.heappush(q,(st + 1,pr + price,to))
-                            
-#         if dst not in t:
-#             return -1
-#         if t[dst][1] > k + 1:
-#             return -1
-#         return t[dst][0]
-    def findCheapestPrice(self,n, flights, src, dst, K):
-        graph = collections.defaultdict(dict)
-        for s, d, w in flights:
-            graph[s][d] = w
-        pq = [(0, src, K+1)]
-        vis = [0] * n
-        while pq:
-            w, x, k = heapq.heappop(pq)
-            if x == dst:
-                return w
-            if vis[x] >= k:
+        stop = 0
+        t = {}
+        visit = set()
+        q = [(0,0,src)]
+        if adj[src] == []:
+            return -1
+        while q:
+            pr,st,node = heapq.heappop(q)
+            # print(pr,st,node)
+            if node == 3:
+                print(pr,st,node)
+                # print(st - 1 , " #")
+            if node in t and t[node][1] <= st:
                 continue
-            vis[x] = k
-            for y, dw in graph[x].items():
-                heapq.heappush(pq, (w+dw, y, k-1))
-        return -1
+            if st - 1  > k:
+                continue
+            if node==dst:  # reach the destination # as priority_queue is a minHeap so this cost is the most minimum cost.
+                return pr
+            t[node] = [pr,st]
+            for to,price in adj[node]:
+                heapq.heappush(q,(pr + price,st + 1,to))
+        print(adj)
+        print(t)
+        if dst not in t:
+            return -1
+        if t[dst][1] > k + 1:
+            return -1
+        return t[dst][0]
+    # n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
