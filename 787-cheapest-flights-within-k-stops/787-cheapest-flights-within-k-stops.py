a06b64b9a85afd8e938f276de43d424554e453a3
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-16 18:01:49 +0300

    Time: 162 ms (77.06%), Space: 15.2 MB (73.36%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -12,24 +12,13 @@ class Solution:
             return -1
         while q:
             pr,st,node = heapq.heappop(q)
-            # print(pr,st,node)
-            if node == 3:
-                print(pr,st,node)
-                # print(st - 1 , " #")
             if node in t and t[node][1] <= st:
                 continue
             if st - 1  > k:
                 continue
-            if node==dst:  # reach the destination # as priority_queue is a minHeap so this cost is the most minimum cost.
+            if node==dst:
                 return pr
             t[node] = [pr,st]
             for to,price in adj[node]:
                 heapq.heappush(q,(pr + price,st + 1,to))
-        print(adj)
-        print(t)
-        if dst not in t:
-            return -1
-        if t[dst][1] > k + 1:
-            return -1
-        return t[dst][0]
-    # n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+        return -1
