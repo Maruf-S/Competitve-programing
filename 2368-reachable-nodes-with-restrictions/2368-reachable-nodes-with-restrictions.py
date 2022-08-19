565a8635397acb4646f5a948d7d15345926e171e
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-19 09:29:23 +0300

    Time: 2591 ms (53.27%), Space: 72.4 MB (73.56%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
@@ -2,7 +2,7 @@ class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
         restricted = set(restricted)
         adj = {i:[] for i in range(n)}
-        visited = set()
+        visited = set([0])
         for s,d in edges:
             adj[s].append(d)
             adj[d].append(s)
@@ -10,10 +10,8 @@ class Solution:
         while q:
             for i in range(len(q)):
                 node = q.popleft()
-                if node in visited:
-                    continue
-                visited.add(node)
                 for nei in adj[node]:
-                    if nei not in restricted:
+                    if nei not in restricted and nei not in visited:
+                        visited.add(nei)
                         q.append(nei)
         return len(visited)
