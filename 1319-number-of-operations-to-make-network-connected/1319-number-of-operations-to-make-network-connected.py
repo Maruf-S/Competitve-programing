5f65b916dd30a77c40f56c65db14eaf08372cbcc
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-20 21:15:51 +0300

    Time: 774 ms (52.64%), Space: 33.8 MB (95.29%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,29 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                p = par[p]
+            return p
+        def union(n1,n2):
+            p1,p2 = find(n1),find(n2)
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                rank[p1] += rank[p2]
+                par[p2] = p1
+            else:
+                rank[p2] += rank[p1]
+                par[p1] = p2
+            return True
+        rank = [1] * n
+        par = [i for i in range(n)]
+        visit = set()
+        extra = 0
+        group = 0
+        for i,j in connections:
+            if union(i,j) == True:
+                n -= 1
+            else:
+                extra += 1
+        return n-1 if extra >= n-1 else -1
