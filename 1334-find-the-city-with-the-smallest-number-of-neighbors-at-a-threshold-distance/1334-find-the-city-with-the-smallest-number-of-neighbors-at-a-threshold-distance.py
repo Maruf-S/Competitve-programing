44d92d74d8d658933eda67c0f293237ee420a37b
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-21 23:58:37 +0300

    Time: 526 ms (83.56%), Space: 15.3 MB (41.38%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,33 +1,41 @@
 class Solution:
-    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        adj = defaultdict(list)
-        for n1, n2, w in edges:
-            adj[n1].append((n2, w))
-            adj[n2].append((n1, w))
-        
-        min_city, min_reach = None, float('inf')
-        for i in range(n):
-            cur_reach = self.findReachByDjikstra(i, adj, distanceThreshold, n)
-            if cur_reach <= min_reach:
-                min_city = i
-                min_reach = cur_reach
-        return min_city
-            
-    def findReachByDjikstra(self, node, adj, distance, n):
-        dest = [float('inf')] * n
-        dest[node] = 0
-        heap = [(0, node)]
-        visited = set()
-        while heap:
-            w1, n1 = heappop(heap)
-            visited.add(n)
-            for n2, w2 in adj[n1]:
-                new_weight = w1 + w2
-                if new_weight <= dest[n2]:
-                    dest[n2] = new_weight
-                    heappush(heap, (new_weight, n2))
-        reach = 0
-        for i in dest:
-            if i <= distance:
-                reach += 1
-        return reach
+    def findTheCity(self, n: int, edges: List[List[int]], k: int) -> int:
+        adj = {i:[] for i in range(n)}
+        for f,t,w in edges:
+            adj[f].append([t,w])
+            adj[t].append([f,w])
+        def getNeighbors(city):
+            minh = [(0,city)]
+            t = {i:float("inf") for i in range(n)}
+            t[city] = 0
+            while minh:
+                w,node = heappop(minh)
+                for nei,wei in adj[node]:
+                        if wei + w < t[nei] :
+                            t[nei] = wei + w
+                            heapq.heappush(minh,(w + wei,nei))
+            res = -1
+            for i in t.values():
+                if i <= k:
+                    res += 1
+            return res
+        return max([(getNeighbors(city), city) for city in range(n)], key=lambda x: (-x[0], x[1]))[-1]
+    # def findReachByDjikstra(self, node, adj, distance, n):
+    #     dest = [float('inf')] * n
+    #     dest[node] = 0
+    #     heap = [(0, node)]
+    #     visited = set()
+    #     while heap:
+    #         w1, n1 = heappop(heap)
+    #         visited.add(n)
+    #         for n2, w2 in adj[n1]:
+    #             if n2 not in visited:
+    #                 new_weight = w1 + w2
+    #                 if new_weight <= dest[n2]:
+    #                     dest[n2] = new_weight
+    #                     heappush(heap, (new_weight, n2))
+    #     reach = 0
+    #     for i in dest:
+    #         if i <= distance:
+    #             reach += 1
+    #     return reach
