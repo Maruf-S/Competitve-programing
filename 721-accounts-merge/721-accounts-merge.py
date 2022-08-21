8e6471f6827b8c692d14fafb5748ea394243e700
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-21 15:13:26 +0300

    Time: 290 ms (67.54%), Space: 17.7 MB (89.88%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,56 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+        
+    def union(self, x, y):
+        findX, findY = self.find(x), self.find(y)
+        if findX != findY:
+            if self.rank[findY] > self.rank[findX]:
+                self.rank[findY] += 1
+                self.parent[findX] = findY
+            else:
+                self.rank[findX] += 1
+                self.parent[findY] = findX
+        
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+        if self.parent[x] == x:
+            return x
+        return self.find(self.parent[x])
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind()
+        user = {}
+        
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                if email in user:
+                    uf.union(i, user[email])
+                user[email] = i
+
+        ans = defaultdict(list)
+        for email, par in user.items():
+            ans[uf.find(par)].append(email)
+        res = []
+        for par, emails in ans.items():
+            res.append([accounts[par][0]] + sorted(emails))
+        return res
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        users = {}
+        uf = UnionFind()
+        for i,(name,*emails) in enumerate(accounts):
+            for email in emails:
+                if email in users:
+                    uf.union(i,users[email])
+                users[email] = i
+        ans = defaultdict(list)
+        for user,par in users.items():
+            ans[uf.find(par)].append(user)
+        res = []
+        for idx, users in ans.items():
+            res.append([accounts[idx][0]] + sorted(users))
+        return res            
