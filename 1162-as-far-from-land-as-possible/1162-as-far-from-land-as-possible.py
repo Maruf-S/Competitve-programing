3ce8cf1657d131c7e2b19faf9d2374870e1150c7
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-21 17:03:04 +0300

    Time: 1178 ms (29.76%), Space: 14.8 MB (51.67%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -5,29 +5,26 @@ class Solution:
         if len(q) == m * n or len(q) == 0: return -1
         dirn = [[1,0],[-1,0],[0,1],[0,-1]]
         level = 0
-#         while q:
-#             for _ in range(len(q)):
-#                 r,c= q.popleft()
-#                 if grid[r][c] == 0:
-#                     # res = max(res,d)
-#                     grid[r][c] = 1
-
-#                 for i,j in dirn:
-#                     r1,c1 = r + i, c + j
-#                     if r1 == m or r1 < 0 or c1 == n or c1  < 0 or grid[r1][c1] == 1:
-#                         continue
-#                     q.append((r1,c1))
-#             level += 1
-#         return level - 1
-        level = 0
         while q:
-            size = len(q)
-            for _ in range(size):
-                i,j = q.popleft()
-                for x,y in dirn:
-                    xi, yj = x+i, y+j
-                    if 0 <= xi < m and 0 <= yj < n and grid[xi][yj] == 0:
-                        q.append((xi, yj))
-                        grid[xi][yj] = 1
+            for _ in range(len(q)):
+                r,c= q.popleft()
+                for i,j in dirn:
+                    r1,c1 = r + i, c + j
+                    if r1 == m or r1 < 0 or c1 == n or c1  < 0 or grid[r1][c1] == 1:
+                        continue
+                    q.append((r1,c1))
+                    grid[r1][c1] = 1
             level += 1
-        return level-1
+        return level - 1
+        # level = 0
+        # while q:
+        #     size = len(q)
+        #     for _ in range(size):
+        #         i,j = q.popleft()
+        #         for x,y in dirn:
+        #             xi, yj = x+i, y+j
+        #             if 0 <= xi < m and 0 <= yj < n and grid[xi][yj] == 0:
+        #                 q.append((xi, yj))
+        #                 grid[xi][yj] = 1
+        #     level += 1
+        # return level-1
