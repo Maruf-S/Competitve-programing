ac4b1d36a5e2a1ec7edcb5aa186484f2d4227e83
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-22 02:46:24 +0300

    Time: 38 ms (80.45%), Space: 14 MB (59.31%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.py
@@ -0,0 +1,25 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = defaultdict(list)
+        for (var1,var2),ans in zip(equations,values):
+            adj[var1].append([var2,ans])
+            adj[var2].append([var1,1/ans])
+        
+        def bfs(st,dst):
+            if st not in adj or dst not in adj:
+                return -1
+            q = deque([(1,st)])
+            visit = set([st])
+            while q:
+                w1, n1 = q.popleft()
+                if n1 == dst:
+                    return w1
+                for n2,w2 in adj[n1]:
+                    if n2 not in visit:
+                        visit.add(n2)
+                        q.append([w2 * w1,n2])
+            return -1
+        res = []
+        for q1,q2 in queries:
+            res.append(bfs(q1,q2))
+        return res
