6b50b05b9859d2816570b4fa6eb513f4768f9858
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-29 08:59:33 +0300

    Time: 69 ms (14.26%), Space: 14.2 MB (35.84%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,25 @@
+# class Solution(object):
+#     def subsets(self, nums):
+#         ret = []
+#         self.dfs(nums, [], ret)
+#         return ret
+    
+#     def dfs(self, nums, path, ret):
+#         ret.append(path)
+#         for i in range(len(nums)):
+#             self.dfs(nums[i+1:], path+[nums[i]], ret)
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
-        def dfs(i):
+        # subset = []
+        def dfs(i,subset):
             if i >= len(nums):
                 return res.append(subset[:])
             subset.append(nums[i])
-            dfs(i + 1)
+            dfs(i + 1,subset)
             
             subset.pop()
-            dfs(i + 1)
-        dfs(0)
+            dfs(i + 1,subset)
+        dfs(0,[])
         return res
