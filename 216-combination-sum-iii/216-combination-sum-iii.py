72431c4588f93fe81f8e449a207abe8a46c92ec2
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-30 22:56:33 +0300

    Time: 58 ms (28.21%), Space: 14 MB (28.70%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,15 +1,37 @@
+# class Solution:
+#     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        
+#         def dfs(i,path,total,k):
+#             if i > 9:
+#                 return
+#             if k == 0:
+#                 if sum(path) == n:
+#                     res.append(path[::])
+#                 return
+#             path.append(i)
+#             dfs(i+ 1,path,total + i,k - 1)
+#             path.pop()
+#             dfs(i + 1,path,total,k)
+#         res = []
+#         dfs(1,[],0,k)
+#         return res
+
+
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = []
-        def dfs(path, summ, idx):
-            if len(path) > k or summ > n:
+        res=[]
+        def backtrack(i,c,r,s):
+            if i==k:
+                if s==n:
+                    res.append(r[:])
+                else:
+                    return
+            if i>=k or c>9:
                 return
-            if summ == n and len(path) == k:
-                res.append(path[:])
-                return
-            for i in range(idx,10):
-                path.append(i)
-                dfs(path, summ+i, i+1)
-                path.pop()
-        dfs([], 0, 1)
+            r.append(c)
+            backtrack(i+1,c+1,r,s+c)
+            r.pop()
+            backtrack(i,c+1,r,s)
+
+        backtrack(0,1,[],0)
         return res
