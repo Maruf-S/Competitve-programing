a9d92a5bcba34c6e59458b09e74493ae32acbc8f
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-30 23:19:27 +0300

    Time: 60 ms (23.60%), Space: 13.9 MB (79.24%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,37 +1,17 @@
-# class Solution:
-#     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        
-#         def dfs(i,path,total,k):
-#             if i > 9:
-#                 return
-#             if k == 0:
-#                 print(path)
-#                 if sum(path) == n:
-#                     res.append(path[:])
-#                 return
-#             path.append(i)
-#             dfs(i+ 1,path,total + i,k - 1)
-#             path.pop()
-#             dfs(i + 1,path,total,k)
-#         res = []
-#         dfs(1,[],0,k)
-#         return res
-
-
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res=[]
-        def backtrack(i,c,path,s):
-            if len(path)==k:
-                if s==n:
+        res = []
+        
+        def dfs(i,path):
+            if len(path) == k:
+                if n == sum(path):
                     res.append(path[:])
                 return
-            if c>9:
+            if i > 9:
                 return
-            path.append(c)
-            backtrack(i+1,c+1,path,s+c)
+            path.append(i)
+            dfs(i + 1,path)
             path.pop()
-            backtrack(i,c+1,path,s)
-
-        backtrack(0,1,[],0)
+            dfs(i + 1,path)
+        dfs(1,[])
         return res
