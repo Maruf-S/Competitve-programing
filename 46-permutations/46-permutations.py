63d1c19ac978d6f6281aabd4639f52dff1dc20b5
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-31 15:37:03 +0300

    Time: 46 ms (86.02%), Space: 14.1 MB (58.40%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtracking(res,visited,subset):
+            if len(subset) == len(nums):
+                res.append(subset)
+            for i in range(len(nums)):
+                if i not in visited:
+                    visited.add(i)
+                    backtracking(res,visited,subset+[nums[i]])
+                    visited.remove(i)
+        visited = set()
+        res = []
+        backtracking(res,visited,[])
+        return res
