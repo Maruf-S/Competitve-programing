c1df80594e27827b3f3def894c78be0da0b8750f
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-08-31 15:54:32 +0300

    Time: 86 ms (8.94%), Space: 14.1 MB (58.40%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -5,6 +5,8 @@ class Solution:
                 res.append(subset[:])
             for i in range(len(nums)):
                 if i not in visited:
+                    if i in visited or (i > 0 and nums[i-1] == nums[i] and not i-1 in visited):
+                        continue
                     visited.add(i)
                     subset.append(nums[i])
                     backtracking(res,visited,subset)
