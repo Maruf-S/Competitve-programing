3161ad837f9c9424b16294db504372403ad5c925
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-01 22:46:57 +0300

    Time: 61 ms (12.57%), Space: 13.9 MB (56.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,16 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        res = 0
-        d = {}
-        def dp(k):
-            nonlocal res
-            if k > n:
-                return 0
-            if k == n:
-                return 1
-            if k + 1 not in d:
-                d[k + 1] = dp(k + 1)
-            if k + 2 not in d:
-                d[k + 2] = dp(k + 2)
-            return d[k + 1] + d[k + 2]
-        return dp(0)
+        one,two = 1,1
+        for i in range(n - 1):
+            one,two = one + two,one
+        return one
