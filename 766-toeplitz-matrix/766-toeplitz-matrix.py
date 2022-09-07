052fd97907b1b680dc745a3b4c8bc89c0437cb64
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-07 19:03:07 +0300

    Time: 126 ms (60.02%), Space: 13.8 MB (98.40%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,16 +1,7 @@
 class Solution:
-    def isToeplitzMatrix(self, arr: List[List[int]]) -> bool:
-          visited = set()
-          rows,cols = len(arr),len(arr[0])
-          def dfs(r,c,val):
-            if r == rows or c == cols or (r,c) in visited:
-              return True
-            if arr[r][c] != val:
-              return False
-            visited.add((r,c))
-            return dfs(r + 1, c + 1,val)
-          for i in range(rows):
-            for j in range(cols):
-              if not dfs(i,j,arr[i][j]):
-                return False
-          return True
+    def isToeplitzMatrix(self, m):
+            for i in range(len(m) - 1):
+                for j in range(len(m[0]) - 1):
+                    if m[i][j] != m[i + 1][j + 1]:
+                        return False
+            return True
