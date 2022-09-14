12ea007d1e65a54787ab30c2d1c740e3bfa6cf10
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-14 19:02:17 +0300

    Time: 2949 ms (5.12%), Space: 52.7 MB (22.26%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,20 +1,33 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+        
+    def union(self, x, y):
+        findX, findY = self.find(x), self.find(y)
+        if findX != findY:
+            self.parent[y] = x
+        
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+            return x
+        p = self.parent[x]
+        while p!= self.parent[p]:
+            # path compression
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+        return p
+    
+
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
-        for i, j in edges:
-            adj[i].append(j)
-        visited = set()
-        res = set()
-        def dfs(i):
-            if i not in visited:
-                visited.add(i)
-                for nei in adj[i]:
-                    if nei not in visited:
-                        dfs(nei)
-                    else:
-                        res.discard(nei)
+        uf = UnionFind()
+        for i,j in edges:
+            uf.union(i,j)
+        res = []
         for i in range(n):
-            if i not in visited:
-                dfs(i)
-                res.add(i)
+            if uf.parent[i] == i:
+                res.append(i)
         return res
