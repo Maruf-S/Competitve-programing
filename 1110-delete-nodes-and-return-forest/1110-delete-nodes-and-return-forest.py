393e4f97f943f2b9ef524ef0744912ccff39278e
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-14 20:44:44 +0300

    Time: 160 ms (10.00%), Space: 14.5 MB (72.09%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -1,30 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        deleteSet = set(to_delete)
         res = []
-        td = set(to_delete)
-        def dfs(node):
-            if not node:
-                return False
-            p = False
-            if node.val in td:
-                p = True
-                if node.left and node.left.val not in td:
-                    res.append(node.left)
-                if node.right and node.right.val not in td:
-                    res.append(node.right)
-            if dfs(node.left):
-                node.left = None
+        def dfs(root, par):
+            if root is None:
+                return None
             
-            if dfs(node.right):
-                node.right = None
-            return p
-        if root.val not in td:
-            res.append(root)
-        dfs(root)
+            if root.val in deleteSet:
+                dfs(root.left, None)
+                dfs(root.right, None)
+                return None
+            
+            root.left = dfs(root.left, root)
+            root.right = dfs(root.right, root)
+            if par is None:
+                res.append(root)
+                return
+            return root
+                  
+        dfs(root, None)
         return res
