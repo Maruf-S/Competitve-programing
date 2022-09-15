3ab477eaada005c6fcd1c7b8c21f9486d66e1447
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-15 17:35:03 +0300

    Time: 540 ms (35.95%), Space: 25.8 MB (13.68%) - LeetHub

--- 403-frog-jump/403-frog-jump.py
+++ 403-frog-jump/403-frog-jump.py
@@ -1,21 +1,14 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
         d = {}
-        def dfs(i,unit,total):
-            if (i,unit,total) in d:
-                return d[(i,unit,total)]
-            if i == len(stones) - 1:
-                if stones[i] == total:
-                    return True
-                else:
-                    return False
-            if total == stones[i]:
-                d[(i,unit,total)] = dfs(i + 1, unit - 1, total + (unit - 1)) or dfs(i + 1,unit,total + unit) or dfs(i + 1,unit + 1,total + (unit + 1))
-            elif total > stones[i]:
-                d[(i,unit,total)] = dfs(i + 1,unit,total)
-            else:
-                d[(i,unit,total)] =  False
-            return d[(i,unit,total)]
-        x = dfs(1,1,1)
+        stoness = set(stones)
+        visited = set()
+        def dfs(val,units):
+            if (val + units) not in stoness or (val, units) in visited:
+                return False
+            visited.add((val,units))
+            if val + units == stones[-1]:
+                return True
+            return dfs(val + units,units) or dfs(val + units,units + 1) or dfs(val + units,units -1)
         # print(len(d))
-        return x
+        return dfs(0,1)
