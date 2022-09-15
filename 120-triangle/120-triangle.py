d7df597554e36c779faaddf702ae7364b9d2fb01
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-15 16:21:59 +0300

    Time: 160 ms (9.45%), Space: 17.5 MB (8.10%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        @lru_cache(maxsize=None)
+        @cache
         def dfs(i,j):
             if i >= len(triangle) or j >= len(triangle[i]):
                 return 0
