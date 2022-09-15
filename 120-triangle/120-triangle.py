1663b5fbf52ffa4fe72dc5405a3469034db5ca46
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-15 16:32:15 +0300

    Time: 162 ms (8.51%), Space: 14.9 MB (93.28%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,5 +3,4 @@ class Solution:
         for i in range(len(triangle) - 2,-1,-1):
             for j in range(len(triangle[i])):
                 triangle[i][j] += min(triangle[i + 1][j],triangle[i + 1][j + 1])
-        print(triangle)
         return (triangle[0][0])
