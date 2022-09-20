5280515f613f4a94352e2f3622b3a84aafdca6cf
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-09-20 15:53:35 +0300

    Time: 2774 ms (16.34%), Space: 27.7 MB (97.86%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        target = sum(nums) - x
+        l = 0
+        curr = 0
+        res = float("inf")
+        for r,val in enumerate(nums):
+            curr += val
+            while curr > target and l <= r:
+                curr -= nums[l]
+                l += 1
+            if curr == target:
+                res = min(res,l + len(nums) - r - 1)
+        return -1 if res == float("inf") else res
+    
+    
+# class Solution:
+#     def minOperations(self, nums: List[int], x: int) -> int:
+#         target = sum(nums) - x
+#         curr_sum, max_len = 0, 0
+#         start_idx = 0
+#         found = False
+#         for end_idx in range(len(nums)):
+#             curr_sum += nums[end_idx]
+#             while start_idx <= end_idx and curr_sum > target:
+#                 curr_sum -= nums[start_idx]
+#                 start_idx += 1
+#             if curr_sum == target:
+#                 found = True
+#                 max_len = max(max_len, end_idx - start_idx + 1)
+
+#         return len(nums) - max_len if found else -1
