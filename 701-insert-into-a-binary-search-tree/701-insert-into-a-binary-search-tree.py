9720921ad1bff56d03c84b921b8e6a1b6867043c
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-13 10:29:21 +0300

    Time: 207 ms (68.31%), Space: 16.9 MB (51.43%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
@@ -1,21 +1,21 @@
-class Solution(object):
-    def insertIntoBST(self, root, val):
-        if(root == None): return TreeNode(val);
-        if(root.val < val): root.right = self.insertIntoBST(root.right, val);
-        else: root.left = self.insertIntoBST(root.left, val);
-        return(root)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root: return TreeNode(val)
-            
-        cur, next = None, root
-        while next:
-            cur = next
-            next = cur.left if val < cur.val else cur.right
-        
-        if val < cur.val: 
-            cur.left = TreeNode(val)
-        else: 
-            cur.right = TreeNode(val)
-            
-        return root
+        node = root
+        while root:
+            if val > root.val:
+                if root.right == None:
+                    root.right = TreeNode(val)
+                    return node
+                root = root.right
+            else:
+                if root.left == None:
+                    root.left = TreeNode(val)
+                    return node
+                root = root.left
+        return TreeNode(val)
