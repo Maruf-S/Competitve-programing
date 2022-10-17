4deb9a934ff53a593cc1e812909f94ad0e7ca381
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-17 17:42:31 +0300

    Time: 4227 ms (5.03%), Space: 14.9 MB (29.92%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        def helper(total,cur,flag):
+            # print(total,cur,flag)
+            if total == n:
+                return 0
+            if total > n or cur > n:
+                return float("inf")
+            if flag:
+                return 1 + helper(total + cur,cur,False)
+            else:
+                return 1 + min(helper(total + cur,cur,False),helper(total,total,True))
+        return helper(0,1,True)
