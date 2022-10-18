ed0cd765b5ad9d3bfa4126d080237470abf844c3
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-18 15:05:39 +0300

    Time: 4706 ms (37.47%), Space: 14 MB (71.43%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,13 +1,18 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
-        if n == 1:
-            return 1
-        num  = [i**2 for i in range(1, int(n**0.5)+1)]
-        @cache
-        def helper(total):
-            if total > n:
-                return float("inf")
-            if total == n:
-                return 0
-            return 1 + min(helper(total + i) for i in num if i <= n)
-        return helper(0)
+    # def numSquares(self, n: int) -> int:
+    #     num  = [i**2 for i in range(1, int(n**0.5)+1)]
+    #     @cache
+    #     def helper(total):
+    #         if total > n:
+    #             return float("inf")
+    #         if total == n:
+    #             return 0
+    #         return 1 + min(helper(total + i) for i in num if i <= n)
+    #     return helper(0)
+    
+    def numSquares(self,n):
+        dp = [0] + [float('inf')]*n
+        for i in range(1, n+1):
+            for j in range(1, int(i**0.5)+1):
+                dp[i] = min(dp[i], 1+dp[i-j*j]) 
+        return dp[n]
