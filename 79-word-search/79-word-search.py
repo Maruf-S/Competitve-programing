d5cdd4f7dfb3218790ee599462376972b382393c
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-20 15:56:35 +0300

    Time: 1799 ms (93.95%), Space: 13.9 MB (51.28%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,22 +1,36 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows,cols = len(board),len(board[0])
-        def dfs(i,j,n):
-            if i >= rows or i < 0 or j < 0 or j >= cols or (i,j) in visited or board[i][j] != word[n]:
+        counter_word = Counter(word)
+        
+        
+        counter_board = Counter()
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                counter_board[board[i][j]] += 1
+                
+        for ele in counter_word:
+            if counter_word[ele] > counter_board[ele]:
                 return False
-            if board[i][j] == word[n] and n == len(word)-1:
+        dirn = [[0,1],[0,-1],[1,0],[-1,0]]
+        rows,cols = len(board),len(board[0])
+        def dfs(i,j,k):
+            if k == len(word):
                 return True
-            visited.add((i,j))
-            res = False
-            if board[i][j] == word[n]:
-                dirn = [[1,0],[-1,0],[0,1],[0,-1]]
-                for r,c in dirn:
-                    if dfs(i + r,j + c,n + 1) == True:
+            if 0 <= i < rows and 0 <= j < cols and (i,j) not in visited and board[i][j] == word[k]:
+                k += 1
+                visited.add((i,j))
+                res = False
+                for i1,j1 in dirn:
+                    if dfs(i + i1,j + j1,k) == True:
                         res = True
-            visited.remove((i,j))
-            return res
+                visited.remove((i,j))
+                return res
+            return False
+        
         visited = set()
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == word[0] and dfs(row,col,0) == True:
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0] and dfs(r,c,0):
                     return True
+        return False
