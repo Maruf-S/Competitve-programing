67581e5507b03af2d2740fb96f5bcdd987c00aef
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-21 10:19:00 +0300

    Time: 4823 ms (22.21%), Space: 30.4 MB (28.90%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,14 +1,22 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2:
+        s = sum(nums)
+        if s % 2:
             return False
-        target = sum(nums)//2
-        nums = nums[::-1]
-        @cache
-        def doSolve(i,total):
-            if i == len(nums):
-                return False
-            if total + nums[i] == target:
-                return True
-            return doSolve(i + 1, total + nums[i]) or doSolve(i + 1, total)
-        return doSolve(0,0)
+        target = s//2
+        dp = [[False for i in range(target + 1)] for i in range(len(nums))]
+        for i in range(len(nums)):
+            dp[i][0] = True
+        for i in range(1,len(dp)):
+            for j in range(len(dp[0])):
+                notPick = dp[i - 1][j]
+                pick = False
+                if j - nums[i] >= 0:
+                    pick = dp[i - 1][j - nums[i]]
+                
+                dp[i][j] = pick or notPick
+                
+        return dp[-1][-1]
+    
+    
+    
