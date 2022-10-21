f3f7a075cebc2d024a9bc80d675dcff1098cef0a
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-21 12:08:40 +0300

    Time: 1165 ms (46.82%), Space: 33.9 MB (86.05%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,30 +1,34 @@
+class UnionFind():
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+    def find(self,x):
+        p = self.parent[x]
+        while p!= self.parent[p]:
+            # path compression
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+        return p
+    def union(self,n1,n2):
+        p1,p2 = self.find(n1),self.find(n2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] < self.rank[p2]:
+            self.rank[p1] += self.rank[p2]
+            self.parent[p2] = p1
+        else:
+            self.rank[p2] += self.rank[p1]
+            self.parent[p1] = p2
+        return True
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        def find(n):
-            p = par[n]
-            while p != par[p]:
-                p = par[p]
-            return p
-        def union(n1,n2):
-            p1,p2 = find(n1),find(n2)
-            if p1 == p2:
-                return False
-            if rank[p1] > rank[p2]:
-                rank[p1] += rank[p2]
-                par[p2] = p1
+        uf = UnionFind(n)
+        redundant, wires = 0, n-1
+        
+        for start, end in connections:
+            if uf.union(start, end):
+                wires -= 1
             else:
-                rank[p2] += rank[p1]
-                par[p1] = p2
-            return True
-        rank = [1] * n
-        par = [i for i in range(n)]
-        visit = set()
-        extra = 0
-        group = 0
-        for i,j in connections:
-            if union(i,j) == True:
-                group += 1
-            else:
-                extra += 1
-        extran = n - group
-        return extran - 1 if extra >= extran - 1 else -1
+                redundant += 1
+
+        return wires if wires <= redundant else -1
