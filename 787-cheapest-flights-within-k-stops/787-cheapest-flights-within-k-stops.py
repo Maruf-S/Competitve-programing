085765516cc8a0a8873bf008c320f23617faf525
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-21 08:43:31 +0300

    Time: 571 ms (25.03%), Space: 15.1 MB (88.58%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,18 +1,11 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        flights_to = defaultdict(list)
-        for fr,to,price in flights:
-            flights_to[fr].append((price,to))
-        
-        heap = [(0,src,0)]
-        visited = set()
-        while heap:
-            cost,dest,stops = heappop(heap)
-            if (dest, stops) in visited or stops > k + 1: 
-                continue
-            visited.add((dest, stops))
-            if dest == dst:
-                return cost
-            for w_nei,nei in flights_to[dest]:
-                heappush(heap,(cost + w_nei, nei,stops + 1))
-        return -1
+        dist = [float("inf")] * n
+        dist[src] = 0
+        for i in range(k + 1):
+            tempDist = dist[:]
+            for fr,to,price in flights:
+                if dist[fr] + price < tempDist[to]:
+                    tempDist[to] = dist[fr] + price
+            dist = tempDist
+        return -1 if dist[dst] == float("inf") else dist[dst]
