55ef48e9bd3876e5919357ce40f9928adf8c17bf
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-21 08:51:03 +0300

    Time: 938 ms (56.29%), Space: 15.9 MB (97.09%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,17 +1,15 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj = defaultdict(list)
-        for u,v,w in times:
-            adj[u].append((w,v))
-        t = {}
-        q = [(0,k)]
-        visit = set()
-        while q:
-            w1,n1 = heapq.heappop(q)
-            if n1 not in visit:
-                t[n1] = w1
-                visit.add(n1)
-                for w,v in adj[n1]:
-                    heappush(q,(w + w1 ,v))
-        return max(t.values()) if len(visit) == n else -1
-
+    def networkDelayTime(self, netTimes: List[List[int]], n: int, k: int) -> int:
+        times = [float("inf")] * n
+        times[k - 1] = 0
+        for _ in range(n - 1):
+            tempTimes = times[:]
+            for fr,to,weight in netTimes:
+                if times[fr - 1] + weight < tempTimes[to - 1]:
+                    tempTimes[to - 1] = times[fr - 1] + weight
+            times = tempTimes
+        max_time = max(times)
+        if max_time == float("inf"):
+            # Unable to reach everyone
+            return -1
+        return max_time
