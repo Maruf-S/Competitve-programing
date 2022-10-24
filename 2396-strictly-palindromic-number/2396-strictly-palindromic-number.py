32f6a9ea1be0b7407ad4c928b7ee76b567fb893d
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-24 17:11:59 +0300

    Time: 50 ms (66.08%), Space: 13.9 MB (14.65%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,3 +1,16 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        return
+        def numberToBase(n, b):
+            if n == 0:
+                return [0]
+            digits = []
+            while n:
+                digits.append(int(n % b))
+                n //= b
+            return digits[::-1]
+        def isPalindrome(s):
+            return s == s[::-1]
+        for i in range(2,n-1):
+            if not isPalindrome(numberToBase(n,i)):
+                return False
+        return True
