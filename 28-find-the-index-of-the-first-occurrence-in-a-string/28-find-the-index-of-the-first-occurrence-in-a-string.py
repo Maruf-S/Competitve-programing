3dfd752d8a5b1aea0d2a3cc48aa7febddbc3aeb0
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-26 13:59:16 +0300

    Time: 40 ms (80.58%), Space: 13.9 MB (17.57%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -6,10 +6,10 @@ class Solution:
             hash_val = 0
             k = len(s) - 1
             for i in range(len(s)):
-                hash_val += ((ord(s[i]) * (11 ** k)))
+                hash_val += ((ord(s[i]) * (31 ** k) % MOD)) % MOD
                 k -= 1
-            return hash_val
-        needle_hash = getHash(needle)
+            return hash_val % MOD
+        needle_hash = getHash(needle) 
         if len(needle) > len(haystack):
             return -1
         window = getHash(haystack[:len(needle)])
@@ -18,10 +18,10 @@ class Solution:
         k = len(needle) 
         n = len(haystack)
         for i in range(1,n - k + 1):
-            window -= (ord(haystack[i - 1]) * (11 ** (k - 1)))
-            window *=11
-            # window %= MOD
-            window += ord(haystack[i + k - 1])
+            window -= (ord(haystack[i - 1]) * (31 ** (k - 1)) % MOD) % MOD
+            window *=31
+            window %= MOD
+            window += ord(haystack[i + k - 1]) % MOD
             if window == needle_hash:
                 return i
             
