791d8833720d88f66700b7fcd48f65ba83e141f0
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-26 15:25:50 +0300

    Time: 1612 ms (70.12%), Space: 33.1 MB (73.31%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        d = {0:-1}
-        psum = 0
-        for i,j in enumerate(nums):
-            psum += j
-            r =  psum % k
-            if r not in d:
-                d[r] = i
-            elif i - d[r] >= 2:
-                return True
-        return False
+        hash = {0:-1}
+        _sum = 0
+        for i in range(len(nums)):
+            _sum += nums[i]
+            # _sum %=k
+            if (_sum % k ) in hash:
+                if i - hash[(_sum % k)]  >= 2:
+                    return True
+            else:
+                hash[_sum % k] = i
+        return False            
+            
