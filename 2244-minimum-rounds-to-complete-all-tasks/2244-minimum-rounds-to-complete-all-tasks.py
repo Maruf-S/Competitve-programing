090f22098079c6ba37776f6b41ede47e163856ce
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-10-30 13:09:01 +0300

    Time: 3132 ms (5.18%), Space: 184.9 MB (5.06%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -6,11 +6,10 @@ class Solution:
             if i == len(tasks):
                 return 0
             res = float("inf")
-            for j in range(i + 1,len(tasks)):
-                if tasks[j] == tasks[i] and (j - i + 1 == 2 or j - i + 1 == 3):
-                    res = min(res,1 + helper(j + 1))
-                else:
-                    break
+            if i + 1 < len(tasks) and tasks[i] == tasks[i + 1]:
+                res = min(res,1 + helper(i + 2))
+            if i  + 2 < len(tasks) and tasks[i] == tasks[i + 2]:
+                res = min(res, 1 + helper(i + 3))
             return res
         ans =  helper(0)
         return ans if ans!=float('inf') else -1
