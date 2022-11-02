25f5abfcf06b22348c8cff10281a204efde89f3e
Maruf-S <56218443+Maruf-S@users.noreply.github.com>
2022-11-02 09:04:00 +0300

    Time: 823 ms (41.59%), Space: 20.6 MB (37.80%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,32 +1,66 @@
+class UF():
+    def __init__(self,n):
+        self.rank = [1] * n
+        self.par = [i for i in range(n)]
+    
+    def find(self,n):
+        while self.par[n] != n:
+            n = self.par[n]
+        return n
+    def union(self,n1,n2):
+        par1 = self.find(n1)
+        par2 = self.find(n2)
+        
+        if par1 == par2:
+            return False
+        if self.rank[par1] > self.rank[par2]:
+            self.rank[par1] += self.rank[par2]
+            self.par[par2] = par1
+        else:
+            self.rank[par2] += self.rank[par1]
+            self.par[par1] = par2
+        return True
+
+            
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        ROW = len(grid)
-        COL = len(grid[0])
-        visited = set()
-        islands = 0
-        def dfs(r,c):
-            if not  r < ROW or r < 0:
-                return
-            if not c < COL or c < 0:
-                return
-            if grid[r][c] == "0" or (r,c) in visited:
-                return
-            visited.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
+        rows,cols = len(grid), len(grid[0])
+        uf = UF(rows * cols)
+        dirn = [[1,0],[-1,0],[0,1],[0,-1]]
+        def isBound(r,c):
+            return 0 <= r < rows and 0 <= c < cols
+        component = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    component += 1
+                    curId = i * cols + j
+                    for i1,j1 in dirn:
+                        nr,nc = i1 + i, j1 + j
+                        newId = nr * cols + nc
+                        if isBound(nr,nc) and grid[nr][nc] == "1":
+                            if uf.union(curId,newId):
+                                component -= 1
+        return component
             
-        for i in range(ROW):
-            for j in range(COL):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    dfs(i,j)
-                    islands += 1
-        return islands
-
-# [["1","1","1","1","0"]
-#  ["1","1","0","1","0"]
-#  ["1","1","0","0","0"]
-#  ["0","0","0","0","0"]]
+# class UnionFind:
+#     def __init__(self):
+#         self.parent = {}
+#         self.rank = {}
+        
+#     def union(self, x, y):
+#         findX, findY = self.find(x), self.find(y)
+#         if findX != findY:
+#             self.parent[y] = x
+        
+#     def find(self, x):
+#         if x not in self.parent:
+#             self.parent[x] = x
+#             self.rank[x] = 0
+#             return x
+#         p = self.parent[x]
+#         while p!= self.parent[p]:
+#             # path compression
+#             self.parent[p] = self.parent[self.parent[p]]
+#             p = self.parent[p]
+#         return p
