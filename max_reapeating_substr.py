5bc53cf5e77217b10c9aae9937bbca1ccc638f63
Maruf Sherefa <56218443+Maruf-S@users.noreply.github.com>
2022-11-17 17:41:05 +0300

    Create max_reapeating_substr.py

---
+++ max_reapeating_substr.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        left = 1
+        right = len(sequence) // len(word)
+        while left <= right:
+            mid = (left + right) // 2
+            if word * mid in sequence:
+                left = mid + 1
+            else:
+                right = mid - 1 
+                
+        return left - 1
