7e838d2d6c6d282ae0403f172de940a3fab600af
Maruf Sherefa <56218443+Maruf-S@users.noreply.github.com>
2023-01-07 10:30:16 +0300

    Time: 36 ms (85.36%), Space: 13.9 MB (65.74%) - LeetHub

--- 843-guess-the-word/843-guess-the-word.py
+++ 843-guess-the-word/843-guess-the-word.py
@@ -1,22 +1,32 @@
 class Solution:
-    def getMatch(self,word1, word2):
-        count = 0
-        for x,y in zip(word1,word2):
-            if x == y:
-                count +=1
-        return count
+    def findSecretWord(self, words: List[str], master: 'Master') -> None:
+        pattern = {}
+        for word in words:
+            for i,c in enumerate(word):
+                pattern[(i,c)] = pattern.get((i,c),0) + 1
+        def calculateProb(word):
+            score = 0
+            for i,c in enumerate(word):
+                score += pattern.get((i,c),0) + 1
+            return score
+        words.sort(key = calculateProb)
+        def canMatch(guess_word,word,matches):
+            if guess_word == word:
+                return False
+            for x,y in zip(guess_word,word):
+                if x == y:
+                    matches -= 1
+            return not matches
+        idx = -1
+        while words:
+            guess0 = words[idx]
+            test = master.guess(guess0)
+            if test == 6:
+                break
+            elif test == 0:
+                words = [w for w in words if not any(a == b for a,b in zip(guess0,w))]
+                idx = -1
+            else:
+                words = [w for w in words if canMatch(guess0,w,test)]
+                idx = 0
                 
-            
-    def findSecretWord(self, wordlist: List[str], master: 'Master') -> None:
-        i = 0
-        matches = 0
-        while i <10 and matches !=6:
-            index = random.randint(0,len(wordlist)-1)
-            word = wordlist[index]
-            matches = master.guess(word)
-            candidates = []
-            for w in wordlist:
-                if matches == self.getMatch(word,w):
-                    candidates.append(w)
-            wordlist = candidates
-        return word
